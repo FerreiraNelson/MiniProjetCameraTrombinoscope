58fa50374f7a50c5ed158684caee64cf46b0c068
Your Name <you@example.com>
2021-11-26 14:22:16 +0100

    Last

---
+++ Interface/mesfonctionscamera.js
@@ -0,0 +1,8 @@
+var inscription = document.getElementById("inscription");
+var connexion = document.getElementById("connexion");
+var nav_inscription = document.getElementById("nav_inscription");
+var nav_connexion = document.getElementById("nav_connexion");
+var divbutton = document.getElementById("divbutton");
+
+divbutton.addEventListener('click', VerifierFormulaireInscription);
+psw1.addEventListener('keyup', VerifierMotDePasse);
